ab5f33fb629dd148a97d517a0868564454c4c4a8
Triknives <cordell.desu@gmail.com>
2020-03-29 20:24:33 -0700

    update structure, add API KEY , and desctructure response array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
+import axips from 'axios';
+
 
 //Import Ionic Styles
 import '@ionic/core/css/core.css';
@@ -17,35 +19,61 @@ import {
   IonCardTitle,
   IonButton
 } from '@ionic/react';
+import Axios from 'axios';
 
 class App extends Component {
+
+  API_KEY = 'd58332f513164373862837a4a93bc475';
+  API_URL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`;
+
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    Axios.get(this.API_URL).then(response => response.data)
+    .then((data) => {
+      this.setState({ items: data.articles })
+      console.log(this.state.items)
+    })
+  }
+
   render() {
     return (
       <IonApp>
       <IonHeader>
         <IonToolbar color="primary">
-          <IonTitle>News App v1.0</IonTitle>
+          <IonTitle>Latest News!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>
-              News title
-            </IonCardTitle>
-            <IonCardSubtitle>
-              News subtitle
-            </IonCardSubtitle>
+      {this.state.items.map((item) => (
+                <IonCard>
+                  <img src={item.urlToImage}/>
+                <IonCardHeader>
 
-          </IonCardHeader>
-          <IonCardContent>
-            <IonButton >Read</IonButton>
-          </IonCardContent>
-        </IonCard>
+                <IonCardTitle>
+                {item.title}
+
+                </IonCardTitle>
+                 <IonCardSubtitle>
+                    {item.author}
+                  </IonCardSubtitle>
+
+                </IonCardHeader>
+                <IonCardContent>
+                  <p>{item.content}</p>
+                  <IonButton href={item.url}> Read</IonButton>
+                </IonCardContent>
+                </IonCard>
+
+      ))}
+
       </IonContent>
     </IonApp>
 
     );
   }
 }
+
 export default App;
